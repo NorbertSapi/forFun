82e18c22d8a78b038518ba0fdd588f74795c4d99
Norbert Sapi <ncs.sapi@gmail.comgit config --global user.emailgit config --global user.emailgit config --global user.email ncs.sapi@gmail.com>
2021-04-17 00:50:06 +0100

    # This is the final of the fun notifier - surprise from me to me.

--- main.py
+++ main.py
@@ -3,10 +3,8 @@
 from datetime import date
 import datetime
 
-# enter your age
-age = input("Enter your age: ")
 
-# today is...
+# today is ...
 today = date.today()
 print("Today's date:", today)
 
@@ -14,8 +12,23 @@ print("Today's date:", today)
 week_days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
 # split the info for year, month and date.
-divide = list(map(int, input("Enter date \n eg: 05/05/2019 \n\n").split('/')))
+divide = list(map(int, input("Enter date \n eg: 2019-05-05 \n\n").split('-')))
 
 # get the weekday
-day = datetime.date(divide[2], divide[1], divide[0]).weekday()
+day = datetime.date(divide[0], divide[1], divide[2]).weekday()
 print(week_days[day])
+current_date = week_days[day]
+
+
+# this is the notifier function, it prints you a notification for the day.
+def notify(week_of_the_day):
+    if week_of_the_day == "Friday":
+        print("This is the last day of the week. Have a beer mate! :D")
+    elif week_of_the_day == "Saturday" or week_of_the_day == "Sunday":
+        print("You still have time for a beer!")
+    else:
+        print("Have Fun! Work Hard! Make History!")
+
+
+# call the function
+notify(current_date)
